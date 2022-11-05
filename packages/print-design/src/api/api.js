62fda1aa5a5bfe12da1582046d0d05a593ed2627
wucw <wu8260@163.com>
2022-11-05 13:37:14 +0800

    移除多余接口

--- packages/print-design/src/api/api.js
+++ packages/print-design/src/api/api.js
@@ -1,24 +1,5 @@
 import { request } from './axios'
 
-// 获取公司信息
-export function GetCompanyInfo(params) {
-  return request({
-    url: '/api/BaseData/GetCompanyInfo',
-    method: 'post',
-    params
-  });
-}
-
-// 获取单据信息
-export function InvoiceDetail(params) {
-  return request({
-    url: '/api/DelegateExternalManage/GetDelegateExternalSendDetailByNo',
-    method: 'post',
-    params,
-    paramsKey: true
-  });
-}
-
 // 保存单据模板
 export function SaveDocumentPrint(params) {
   return request({
