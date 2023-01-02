42b76bcfbc2254223a30e1a5e7f2a1b9c5db6935
wucw <wu8260@163.com>
2023-01-02 11:18:32 +0800

    修改token失效处理逻辑

--- packages/print-design/src/api/axios.js
+++ packages/print-design/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { showMessage } from "./status";
 import { ElMessage } from 'element-plus';
 import { showFullScreenLoading as showLoading, hideFullScreenLoading as hideLoading } from 'common/src/utils/serviceLoading';
-import { storeKey } from "vuex";
+import store from '@/store';
 
 class HttpRequest {
   constructor(baseURL, timeout=10000) {
@@ -25,58 +25,17 @@ class HttpRequest {
         return Promise.reject(error);
       }
     );
-    
-    //是否正在刷新Token
+
     let isRefreshToken = false;
-    // 缓存请求队列
     let refreshRequest = [];
 
     this.instance.interceptors.response.use(
       response => {
-        if(!isRefreshToken) {
-          isRefreshToken = true;
-          return storeKey.dispatch('user/refreshToken').then(data => {
-            if (data.token) {
-              sessionStorage.setItem('accessToken');
-              // 更新请求头
-              const params = response.config.params
-              if (params && params.token) {
-                params.token = data.token
-              }
-              refreshRequest.forEach(callback=>{
-                callback(data.token)
-              })
-              return request(response.config)
-            } else {
-              // 刷新失败需要重新登录 
-              sessionStorage.removeItem('accessToken');
-              location.reload();
-              return Promise.reject()
-            }
-          }).catch(() => {
-            // 刷新失败需要重新登录 
-            sessionStorage.removeItem('accessToken');
-            location.reload();
-          }).finally(() => {
-            isRefreshToken=false
-          })
-        } else {
-          // 缓存请求列表(利用Promise防止之前的请求返回错误的Token导致的错误信息)
-          return new Promise((resolve) => {
-            // 将resolve放进队列，等token刷新后直接执行
-            refreshRequest.push((token) => {
-              // 更新请求头
-              response.config.headers['Account-Token'] = sessionStorage.getItem('accessToken')
-              resolve(service(response.config));
-            });
-          });
+        return {
+          data: response.data,
+          status: response.status,
+          msg: response.statusText
         }
-
-        // return {
-        //   data: response.data,
-        //   status: response.status,
-        //   msg: response.statusText
-        // }
       },
 
       error => {
@@ -88,6 +47,43 @@ class HttpRequest {
             location.reload();
             return;
           }
+
+          // if (response.data.code === -101) {
+          //   if (!isRefreshToken) {
+          //     isRefreshToken = true;
+          //     const refreshToken = sessionStorage.getItem('refreshToken');
+          //     return store.dispatch('user/updateToken', refreshToken).then(data => {
+          //       if (data.accessToken) {
+          //         refreshRequest.forEach(callback => {
+          //           callback(data.accessToken);
+          //         });
+          //         return service(response.config);
+          //       } else {
+          //         // 刷新失败需要重新登录
+          //         store.dispatch('user/resetToken').then(() => {
+          //           location.reload();
+          //         });
+          //         return Promise.reject();
+          //       }
+          //     }).catch(err => {
+          //       // 刷新失败需要重新登录
+          //       store.dispatch('user/resetToken').then(() => {
+          //         location.reload();
+          //       });
+          //     }).finally(() => {
+          //       isRefreshToken = false;
+          //     });
+          //   } else {
+          //     return new Promise(resolve => {
+          //       // 将resolve放进队列，等token刷新后直接执行
+          //       refreshRequest.push((token) => {
+          //         // 更新请求头
+          //         response.config.headers['Account-Token'] = token;
+          //         resolve(service(response.config));
+          //       });
+          //     });
+          //   }
+          // }
 
           const msg = showMessage(response.status)
           ElMessage.error(response.data)
